a68bb8bc5f97f89426c748c942f1492d0633c83f
Sangtong Peesing <Sangtong Peesing>
2016-06-19 19:25:27 +0700

    Configured setup.cfg file to support searching all places.

--- config/setup.php
+++ config/setup.php
@@ -4,7 +4,7 @@ return [
     'map' => [ // This is settings for Google map API.
         'url'       => 'https://maps.googleapis.com/maps/api/js?libraries=places', // API URL
         'key'       => '', // API key
-        'zoom'      => 20, // Zoom value
+        'zoom'      => 15, // Zoom value
     ],
     'twitter' => [ // This is settings for Twitter API.
         'url'               => 'https://api.twitter.com/1.1/search/tweets.json', // API URL
@@ -12,7 +12,7 @@ return [
         'accessTokenSecret' => '', //  Access Token Secret
         'consumerKey'       => '', // Consumer Key ( API Key )
         'consumerSecret'    => '', // Consumer Secret ( API Secret )
-        'radius'            => '5km', // Radius
+        'radius'            => '15km', // Radius
         'resultType'        => 'mixed', // Result type of Twitter response ( 'mixed', 'recent', 'popular' )
         'cacheInterval'     => 60, // Keep Twitter response in cache. A unit of time is minute.
     ],
